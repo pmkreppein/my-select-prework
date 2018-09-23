583e4ad9b252663f4fd1d88a3fd2abb1b17de1d0
Peter Kreppein <pmkreppein@gmail.com>
2018-09-23 22:00:58 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,9 +3,10 @@ def my_select(collection)
   output_array = []
   while i < collection.length
     if yield(collection[i]) 
-      output_array << collection[i]
+      output_array << yield(collection[i])
     else
       next
     end
+  end
     output_array
 end
